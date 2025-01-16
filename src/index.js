06423dd6658c00c8966f66d3aa9f0e6295e974c0
tylersemel <tylersemel@gmail.com>
2025-01-16 13:56:33 -0500

    Fix styling so that home page does not use absolute positioning

--- src/index.js
+++ src/index.js
@@ -2,9 +2,37 @@ import "./styles.css";
 import { initalPageLoader } from "./initial-page-loader.js";
 import { menuLoader } from "./menu-loader.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    // initalPageLoader.loadPage();
-    menuLoader.loadPage();
-});
+const nav = document.querySelector('header nav');
+nav.addEventListener('click', handleNavClick, false);
 
-console.log(menuLoader.loadPage);
+function handleNavClick(event) {
+    switch(event.target.className) {
+        case 'home':
+            clearModule();
+            initalPageLoader.loadPage();
+            break;
+        case 'menu':  
+            clearModule();
+            menuLoader.loadPage();  
+            break;
+        case 'about':
+            break;
+        default:
+            break;
+    }
+}
+
+function clearModule() {
+    const contentDiv = document.querySelector('#content');
+    console.log(contentDiv.childNodes.length);
+
+    for (let i = 0; i < contentDiv.childNodes.length; i++) {
+        contentDiv.removeChild(contentDiv.childNodes[i]);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // initalPageLoader.loadPage();
+    // menuLoader.loadPage();
+});
